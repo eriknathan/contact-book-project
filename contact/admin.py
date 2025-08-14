c7a70fcc8bd300ff1a54df304cdd3fa0052859fc
eriknathan <eriknathan.contato@gmail.com>
2025-08-14 20:09:02 -0300

    adding models and admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'first_name', 'last_name',
--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / 'templates',
+            BASE_DIR / 'base_templates',
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -100,9 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -114,9 +114,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+    BASE_DIR / 'base_static',
 )
+STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
